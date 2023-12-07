013b239b41a386121ebeee6c52726331a17fc48a
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-08 00:13:40 +0100

    avancement pages mission

--- js/index.js
+++ js/index.js
@@ -2,3 +2,30 @@
 function navigateToPage(page) {
     window.location.href = page;
 }
+
+// Déplace le script en bas du fichier juste avant la balise de fermeture du body
+document.addEventListener('DOMContentLoaded', function () {
+    const menuLinks = document.querySelectorAll('#menu a');
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const menuNumber = parseInt(link.getAttribute('data-menu'));
+            showNextMenu(menuNumber);
+        });
+    });
+});
+
+function showNextMenu(menuNumber) {
+    // Masque tous les menus
+    const menus = document.querySelectorAll('.menu');
+    menus.forEach(menu => {
+        menu.classList.remove('visible');
+    });
+
+    // Affiche le menu spécifié
+    const currentMenu = document.getElementById(`mission${menuNumber}`);
+    if (currentMenu) {
+        currentMenu.classList.add('visible');
+    }
+}
